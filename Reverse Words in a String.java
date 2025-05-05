89a6c028518f2839aedf9431e4285db3a5073e66
Ömer Sarıtoy <101520451+omersaritoy@users.noreply.github.com>
2025-05-05 22:16:11 +0300

    Solution Problems

---
+++ Reverse Words in a String.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String reverseWords(String s) {
+    String[] words = s.trim().split("\\s+");
+    s="";
+    for (int i = words.length-1; i >=0; i--) {
+        if(i==0)
+            s+=words[i];
+        else
+            s+=words[i]+" ";
+
+    }
+    return s;
+}
+}
