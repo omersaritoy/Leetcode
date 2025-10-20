c964f3935034e43545681a01089e2b7591580ee5
Ömer Sarıtoy <101520451+omersaritoy@users.noreply.github.com>
2025-10-20 14:50:12 +0300

    Time: 17 ms (11.34%) | Memory: 45 MB (27.37%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,8 +1,17 @@
 class Solution {
     public boolean isPalindrome(String s) {
         s= s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
-        StringBuilder sb = new StringBuilder(s).reverse();
-
-        return sb.toString().equals(s);
+        int i=0;
+        int j=s.length()-1;
+     
+        System.out.println(s);
+        while(j>=0){
+            if(s.charAt(i)!=s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
     }
 }
